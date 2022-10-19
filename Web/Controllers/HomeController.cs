5e985befef45151d89937bf10f6e181c1c9a22a0
seba2019 <mrsavl1@gmail.com>
2022-10-19 00:04:03 -0300

    Visitas vm

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -9,26 +9,29 @@ using Web.Models;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
   
-        private readonly List<Personas> __listPersonas;
+        private readonly List<Personas> _listPersonas;
         private readonly List<Renaper> _renapers;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly List<VisitasVm> _listVisitasRandom;
 
         public HomeController(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
-            string pathJsonPersonas = Path.Combine(_webHostEnvironment.WebRootPath, "Data/Personas.json");
-            string pathJsonRenaper = Path.Combine(_webHostEnvironment.WebRootPath, "Data/Renaper.json");
+            _listVisitasRandom = new List<VisitasVm>();
+            string pathJsonPersonas = Path.Combine(_webHostEnvironment.ContentRootPath, "Data/Personas.json");
+            string pathJsonRenaper = Path.Combine(_webHostEnvironment.ContentRootPath, "Data/Renaper.json");
 
             using (StreamReader jsonStreamPersonas = System.IO.File.OpenText(pathJsonPersonas))
             {
                 var jsonPersonas = jsonStreamPersonas.ReadToEnd();
-                __listPersonas = JsonConvert.DeserializeObject<List<Personas>>(jsonPersonas);
+                _listPersonas = JsonConvert.DeserializeObject<List<Personas>>(jsonPersonas);
             }
 
             using (StreamReader jsonStreamRenaper = System.IO.File.OpenText(pathJsonRenaper))
@@ -36,11 +39,30 @@ namespace Web.Controllers
                 var jsonRenaper = jsonStreamRenaper.ReadToEnd();
                 _renapers = JsonConvert.DeserializeObject<List<Renaper>>(jsonRenaper);
             }
+
+
+            Random rd = new Random();
+            int posRandom = 0;
+            Personas persona = new Personas();
+            
+            for (int i = 0; i < 10; i++) 
+            {
+                posRandom = rd.Next(0, _listPersonas.Count - 1);
+                persona = _listPersonas[posRandom];
+                VisitasVm visita = new VisitasVm()
+                {
+                    Id = i + 1,
+                    PersonaVisitada = $"{persona.Nombre} {persona.Apellido}",
+                    Hora = DateTime.Today.ToShortTimeString(),
+                    Fecha = DateTime.Today.Date.ToShortDateString()
+                };
+                _listVisitasRandom.Add(visita);
+            }
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_listVisitasRandom);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Web/ViewModels/VisitasVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class VisitasVm
+    {
+        public int Id { get; set; }
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public string PersonaVisitada { get; set; }
+        
+    }
+}
