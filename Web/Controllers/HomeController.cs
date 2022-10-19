2c8fadc7d7beb79cd6c71f70a39647e5c2492a51
seba2019 <mrsavl1@gmail.com>
2022-10-18 23:25:55 -0300

    Initial commit

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+  
+        private readonly List<Personas> __listPersonas;
+        private readonly List<Renaper> _renapers;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            string pathJsonPersonas = Path.Combine(_webHostEnvironment.WebRootPath, "Data/Personas.json");
+            string pathJsonRenaper = Path.Combine(_webHostEnvironment.WebRootPath, "Data/Renaper.json");
+
+            using (StreamReader jsonStreamPersonas = System.IO.File.OpenText(pathJsonPersonas))
+            {
+                var jsonPersonas = jsonStreamPersonas.ReadToEnd();
+                __listPersonas = JsonConvert.DeserializeObject<List<Personas>>(jsonPersonas);
+            }
+
+            using (StreamReader jsonStreamRenaper = System.IO.File.OpenText(pathJsonRenaper))
+            {
+                var jsonRenaper = jsonStreamRenaper.ReadToEnd();
+                _renapers = JsonConvert.DeserializeObject<List<Renaper>>(jsonRenaper);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/Models/Personas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class Personas
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Sector { get; set; }
+
+    }
+}
